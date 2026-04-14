87e199dbf9fef33fb5764c42528a2c609d89143c
urvesh-broadreach <urvesh@brdrch.com>
2026-04-14 09:15:43 -0400

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { mainData } from './data/main';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +12,63 @@ import { CommonModule } from '@angular/common';
 export class AppComponent implements OnInit {
   
   data = mainData;
+  isDarkMode = true;
+  currentSlide = 0;
+  sliderTimer: any;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
-    //throw new Error('Method not implemented.');
+    if (isPlatformBrowser(this.platformId)) {
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme === 'light') {
+        this.isDarkMode = false;
+        this.renderer.addClass(document.body, 'light-theme');
+      }
+      this.startSlider();
+    }
   }
 
+  startSlider() {
+    this.sliderTimer = setInterval(() => {
+      this.nextSlide();
+    }, 6000);
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.data.mainSlider.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.data.mainSlider.length) % this.data.mainSlider.length;
+  }
+
+  resetSlider() {
+    if (this.sliderTimer) {
+      clearInterval(this.sliderTimer);
+    }
+    this.startSlider();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    console.log('Toggling theme. Dark mode:', this.isDarkMode);
+    
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.isDarkMode) {
+        this.renderer.removeClass(document.body, 'light-theme');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        this.renderer.addClass(document.body, 'light-theme');
+        localStorage.setItem('theme', 'light');
+      }
+      
+      // Verify the class state
+      console.log('Body classes:', document.body.className);
+    }
+  }
 
 }
